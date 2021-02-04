886e6e72ca73f3094139f48bdcab49728b528a7e
Hasan Imam <hasanimam200097@gmail.com>
2021-02-04 22:16:59 +0600

    Crude for question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,9 +9,16 @@ use App\Model\Category;
 
 class Question extends Model
 {
+    
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+    //protected $fillable = ['title','slug','body','category_id','user_id'];
+    protected $guarded = [];
     public function user()
     {
-        return $this->belonsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function reply()
@@ -22,6 +29,11 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");    
     }
 
 }
